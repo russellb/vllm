3e321257afe734b8a7566304d33ac6e67adbf71d
Michal Moskal <michal@moskal.me>
2025-03-18 21:04:07 +0000

    use new llg 0.7.1 APIs; simplify

--- vllm/model_executor/guided_decoding/guidance_decoding.py
+++ vllm/model_executor/guided_decoding/guidance_decoding.py
@@ -1,43 +1,46 @@
 # SPDX-License-Identifier: Apache-2.0
 from re import escape as regex_escape
-from typing import Union
+from typing import Optional
 
 from transformers import PreTrainedTokenizerBase
 
 from vllm.model_executor.guided_decoding.guidance_logits_processors import (
-    ChoiceGuidanceLogitsProcessor, GrammarGuidanceLogitsProcessor,
-    GuidanceLogitsProcessor, JsonGuidanceLogitsProcessor,
-    RegexGuidanceLogitsProcessor)
+    GuidanceLogitsProcessor)
 from vllm.sampling_params import GuidedDecodingParams
+
+import llguidance
 
 
 def get_local_guidance_guided_decoding_logits_processor(
-    guided_params: GuidedDecodingParams, tokenizer: PreTrainedTokenizerBase
-) -> Union[GuidanceLogitsProcessor, None]:
+        guided_params: GuidedDecodingParams,
+        tokenizer: PreTrainedTokenizerBase) -> GuidanceLogitsProcessor:
     """
     Given an OpenAI-compatible request, check for guided decoding parameters
     and get the necessary logits processor for the given guide.
-    We cache logit processors by (guide, tokenizer), and on cache hit
-    we make a shallow copy to reuse the same underlying FSM.
     """
 
+    grm = ""
     if guided_params.json:
-        return JsonGuidanceLogitsProcessor(guided_params.json, tokenizer,
-                                           guided_params.whitespace_pattern)
+        grm = llguidance.LLMatcher.grammar_from_json_schema(
+            guided_params.json,
+            overrides={"whitespace_pattern": guided_params.whitespace_pattern})
     elif guided_params.json_object:
-        return JsonGuidanceLogitsProcessor('{"type": "object"}', tokenizer,
-                                           guided_params.whitespace_pattern)
+        grm = llguidance.LLMatcher.grammar_from_json_schema(
+            '{"type": "object"}',
+            overrides={"whitespace_pattern": guided_params.whitespace_pattern})
     elif guided_params.regex:
-        return RegexGuidanceLogitsProcessor(guided_params.regex, tokenizer,
-                                            None)
+        grm = llguidance.grammar_from("regex", guided_params.regex)
     elif guided_params.choice:
         # choice just uses regex
         choices = (regex_escape(str(choice))
                    for choice in guided_params.choice)
         choices_regex = "(" + "|".join(choices) + ")"
-        return ChoiceGuidanceLogitsProcessor(choices_regex, tokenizer, None)
+        grm = llguidance.grammar_from("regex", choices_regex)
     elif guided_params.grammar:
-        return GrammarGuidanceLogitsProcessor(guided_params.grammar, tokenizer,
-                                              None)
+        # this supports Lark and GBNF
+        grm = llguidance.grammar_from("grammar", guided_params.grammar)
+
+    if grm:
+        return GuidanceLogitsProcessor(grm, tokenizer)
 
     raise ValueError("Unknown guided decoding mode")

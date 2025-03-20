800508a514a7f428e105ec046d15266b1fada56a
Michal Moskal <michal@moskal.me>
2025-03-20 00:36:18 +0000

    adapt to new LLMatcher API

--- vllm/v1/structured_output/backend_guidance.py
+++ vllm/v1/structured_output/backend_guidance.py
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-import json
 import os
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
@@ -48,43 +47,41 @@ class GuidanceBackend(StructuredOutputBackend):
                         grammar_spec: str) -> StructuredOutputGrammar:
 
         if request_type == StructuredOutputOptions.JSON:
-            if isinstance(grammar_spec, dict):
-                schema = json.dumps(grammar_spec)
-            else:
-                schema = str(grammar_spec)
-
             # TODO: make whitespace_flexible configurable
-            compiler = llguidance.JsonCompiler(whitespace_flexible=False)
-            self.serialized_grammar = compiler.compile(schema)
+            self.serialized_grammar = llguidance.LLMatcher.grammar_from_json_schema(
+                grammar_spec, defaults={
+                    "whitespace_flexible": True,
+                })
         elif request_type == StructuredOutputOptions.JSON_OBJECT:
-            compiler = llguidance.JsonCompiler(whitespace_flexible=False)
-            self.serialized_grammar = compiler.compile('{"type": "object"}')
-        elif (request_type == StructuredOutputOptions.REGEX
-              or request_type == StructuredOutputOptions.CHOICE):
-            compiler = llguidance.RegexCompiler()
-            self.serialized_grammar = compiler.compile(regex=grammar_spec)
-        elif request_type == StructuredOutputOptions.GRAMMAR:
-            if isinstance(grammar_spec, dict):
-                self.serialized_grammar = json.dumps(grammar_spec)
-            else:
-                self.serialized_grammar = str(grammar_spec)
+            self.serialized_grammar = llguidance.LLMatcher.grammar_from_json_schema(
+                '{"type": "object"}', defaults={
+                    "whitespace_flexible": True,
+                })
         else:
-            logger.error(
-                "Validation should have already occurred. Please file an issue."
-            )
-            raise ValueError(
-                f"grammar is not of valid supported types. ({request_type!s})")
+            if request_type == StructuredOutputOptions.REGEX:
+                tp = "regex"
+            elif request_type == StructuredOutputOptions.GRAMMAR:
+                tp = "grammar"
+            elif request_type == StructuredOutputOptions.CHOICE:
+                tp = "choice"
+            else:
+                logger.error(
+                    "Validation should have already occurred. Please file an issue."
+                )
+                raise ValueError(
+                    f"grammar is not of valid supported types. ({request_type!s})"
+                )
 
-        ll_interpreter = llguidance.LLInterpreter(
+            self.serialized_grammar = llguidance.grammar_from(tp, grammar_spec)
+
+        ll_matcher = llguidance.LLMatcher(
             self.ll_tokenizer,
             self.serialized_grammar,
-            enable_backtrack=False,
-            enable_ff_tokens=False,
             log_level=int(os.environ.get("LLGUIDANCE_LOG_LEVEL", "1")),
         )
 
         return GuidanceGrammar(
-            ll_interpreter=ll_interpreter,
+            ll_matcher=ll_matcher,
             ll_tokenizer=self.ll_tokenizer,
             vocab_size=self.vocab_size,
         )
@@ -96,44 +93,36 @@ class GuidanceBackend(StructuredOutputBackend):
 
 @dataclass
 class GuidanceGrammar(StructuredOutputGrammar):
-
-    ll_interpreter: llguidance.LLInterpreter
-    ll_tokenizer: llguidance_hf.LLTokenizer
+    ll_matcher: llguidance.LLMatcher
+    ll_tokenizer: llguidance.LLTokenizer
     vocab_size: int
-    stopped: bool = False
 
     def accept_tokens(self, request_id: str, tokens: list[int]) -> bool:
-        """Accepts a list of tokens and advances the FSM.
+        """Accepts a list of tokens and advances the parser.
 
-        Returns True if the FSM was advanced successfully.
-        Returns False if the FSM failed to advance.
+        Returns True if the parser was advanced successfully.
+        Returns False if the parser failed to advance.
         """
 
-        if self.stopped:
+        if self.ll_matcher.is_stopped():
             return True
 
-        for token in tokens:
-            # TODO - Add jump decoding support in the future.
-            # For now we turn this off when creating the LLInterpreter.
-            #backtrack, ff_tokens = self.ll_interpreter.commit_token(token)
-            self.ll_interpreter.commit_token(token)
+        # TODO - Add jump decoding support in the future:
+        # self.ll_matcher.compute_ff_bytes() - this should always work
+        # self.ll_matcher.compute_ff_tokens() - this only works for "canonical" tokenizers
+        # for conversion between the two, see https://github.com/guidance-ai/llguidance/blob/main/docs/fast_forward.md
 
-        return True
+        r = self.ll_matcher.consume_tokens(tokens)
+
+        return r
 
     def fill_bitmask(self, bitmask: torch.Tensor, idx: int) -> None:
-        if self.ll_interpreter.has_pending_stop():
-            # fill bitmask with eos token before is_terminated() return True
-            eos_token = self.ll_tokenizer.eos_token
-            bitmask[idx, :] = 0
-            bitmask[idx, eos_token // 32] = 1 << (eos_token % 32)
-            self.stopped = True
-        else:
-            llguidance_torch.fill_next_token_bitmask(self.ll_interpreter,
-                                                     bitmask, idx)
+        # this will automatically return [EOS] mask if the matcher is stopped or otherwise in an error state
+        llguidance_torch.fill_next_token_bitmask(self.ll_matcher, bitmask, idx)
 
     def is_terminated(self) -> bool:
-        return self.stopped
+        return self.ll_matcher.is_stopped()
 
     def reset(self):
         # This method may be not needed anymore? TODO
-        pass
+        self.ll_matcher.reset()

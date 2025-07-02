1ee1b9c4d54ace0b59f6b45b77ee9c4020855bb4
Russell Bryant <rbryant@redhat.com>
2025-07-02 02:49:08 +0000

    gpu_model_runner: Don't run encoder directly for encoder-decoder
    For multimodal models, the encoder is run directly first so that the
    result can be reused potentially multiple times when being substituted
    into the decoder prompt. This case is not relevant for
    encoder-decoder, so it's simpler to just execute the model as usual
    and let the encoder run during the first forward() call to the model.
    It will only run the first time once everything is working properly.
    
    Signed-off-by: Russell Bryant <rbryant@redhat.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -1316,14 +1316,16 @@ class GPUModelRunner(LoRAModelRunnerMixin):
 
         # _prepare_inputs may reorder the batch, so we must gather multi
         # modal outputs after that to ensure the correct order
-        if self.is_multimodal_model:
+        if (self.is_multimodal_model
+                and not self.model_config.is_encoder_decoder):
             # Run the multimodal encoder if any.
             self._execute_mm_encoder(scheduler_output)
             mm_embeds = self._gather_mm_embeddings(scheduler_output)
         else:
             mm_embeds = []
 
-        if self.is_multimodal_model and get_pp_group().is_first_rank:
+        if self.is_multimodal_model and get_pp_group().is_first_rank and (
+                not self.model_config.is_encoder_decoder):
             # NOTE(woosuk): To unify token ids and soft tokens (vision
             # embeddings), we always use embeddings (rather than token ids)
             # as input to the multimodal model, even when the input is text.
